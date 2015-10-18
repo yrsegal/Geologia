193af9d3d5e02f4848b9529b6e91dea145fb8b16
yrsegal <yrsegal@gmail.com>
2015-10-18 15:09:44 -0400

    rockbase

--- src/main/java/net/nucleria/GeologyMod/common/item/ItemRockPure.java
+++ src/main/java/net/nucleria/GeologyMod/common/item/ItemRockPure.java
@@ -1,51 +1,7 @@
 package net.nucleria.GeologyMod.common.item;
 
-import java.util.List;
+public class ItemRockPure extends ItemRockBase {
 
-import net.minecraft.client.renderer.texture.IIconRegister;
-import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.IIcon;
-import net.nucleria.GeologyMod.GeologyMod;
+	private static String type = "pure";
 
-public class ItemRockPure extends Item {
-
-	public ItemRockPure(){
-		super();
-		
-		this.setHasSubtypes(true);
-		this.setCreativeTab(CreativeTabs.tabMaterials);
-		this.setUnlocalizedName("rock_pure");
-	}
-	
-	public IIcon[] icons = new IIcon[6];
-	
-	@Override
-	public void registerIcons(IIconRegister reg){
-		for (int i = 0; i < 6; i++) {
-			this.icons[i] = reg.registerIcon(GeologyMod.MODID + ":pure_rock_" + i);
-		}
-	}
-	
-	@Override
-	public IIcon getIconFromDamage(int meta){
-		if (meta > 4)
-			meta = 0;
-		
-		return this.icons[meta];
-	}
-	
-	@Override
-	public void getSubItems(Item item, CreativeTabs tab, List list){
-		for (int i = 0; i < 4; i++){
-			list.add(new ItemStack(item, 1, i));
-		}
-	}
-	
-	@Override
-	public String getUnlocalizedName(ItemStack stack) {
-		return this.getUnlocalizedName() + "_" + stack.getItemDamage();
-	}
-	
 }
